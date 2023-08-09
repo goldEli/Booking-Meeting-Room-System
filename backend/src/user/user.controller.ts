c8d9ba41eea3357a2c9d5c39aebedde28b6e30ed
缪宇 <miaoyu2009@qq.com>
2023-08-09 15:01:10 +0800

    feat: register

--- backend/src/user/user.controller.ts
+++ backend/src/user/user.controller.ts
@@ -1,7 +1,14 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post, Body } from '@nestjs/common';
 import { UserService } from './user.service';
+import { RegisterUserDto } from './dto/register-user.dto';
 
 @Controller('user')
 export class UserController {
   constructor(private readonly userService: UserService) {}
+
+  @Post('register')
+  register(@Body() registerUser: RegisterUserDto) {
+    console.log(registerUser);
+    return 'success';
+  }
 }
